dfed8ea127db9e3f77ccac42bb717e3335e79ad4
Diego <juandiegoaguilarok@gmail.com>
2025-06-10 10:30:02 -0500

    agregar componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected title = 'todoapp';
+  protected title = 'Mi primera aplicación Angular';
+  protected tasks = [
+   'Create un proyecto Angular',
+    'Crear un componente',
+    'Actualización de la vista',
+  ];
 }
