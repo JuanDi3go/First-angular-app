2493c37bcdd001ff2a65db2c0e749c820f7f18c9
Diego <juandiegoaguilarok@gmail.com>
2025-06-11 08:08:51 -0500

    Add Tasks and remove tasks

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+    protected tasks = signal([
+   'Create un proyecto Angular',
+    'Crear un componente',
+    'Actualización de la vista',
+    ]);
+
+    protected changeHandler = (event: Event) => {
+
+      const input = event.target as HTMLInputElement;
+      const newTask = input.value;
+
+      this.tasks.update((tasks) => [...tasks,newTask]);
+
+      input.value = "";
+
+    }
+
+    protected onDeleteTaskHandler = (index: number) => {
+
+      this.tasks.update((tasks) => tasks.filter((task,position) => position != index));
+
+    }
 
 }
--- src/app/pages/labs/labs.ts
+++ src/app/pages/labs/labs.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -8,18 +9,37 @@ import { CommonModule } from '@angular/common';
   styleUrl: './labs.css'
 })
 export class Labs {
-protected tasks = [
+protected tasks = signal([
    'Create un proyecto Angular',
     'Crear un componente',
     'Actualización de la vista',
-  ];
-protected name = 'Angular Labs';
-protected disabled = true;
+    ]);
+protected name = signal("Angular labs");
+protected disabled = false;
 protected imageUrl = 'https://w3schools.com/howto/img_avatar.png';
 protected person = {
   name: 'John Doe',
   age: 30,
   avatar: 'https://w3schools.com/howto/img_avatar.png'
+};
+
+protected clickHandler = () => {
+  alert(this.person.name+ ' user')
+};
+
+protected changeEvent = (event: Event) => {
+
+  const elementInput = event.target as HTMLInputElement;
+  
+  this.person.age = +elementInput.value;
+  
+};
+
+protected keyDownEvent = (event: Event) => {
+
+  const elementInput = event.target as HTMLInputElement;
+  console.log(elementInput.value);
+  
 }
 
 }
